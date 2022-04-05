50c4b6a2c13ff5c46a8f3ad3c5a839238d3abc79
Sudan23 <pudasaini.sudan10@gmail.com>
2022-04-05 07:22:18 +0545

    RecyclerView Adapater created

---
+++ app/src/main/java/np/com/sudan10/recyclerview/RecyclerViewAdapter.kt
@@ -0,0 +1,32 @@
+package np.com.sudan10.recyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.NonNull
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerViewAdapter(private var itemsList: List<String>):
+    RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>()
+    {
+    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var itemTextView: TextView = view.findViewById(R.id.itemTextView)
+    }
+    @NonNull
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val item = itemsList[position]
+        holder.itemTextView.text = item
+    }
+
+    override fun getItemCount() = itemsList.size
+
+}
